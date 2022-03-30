39b65de54d80a13a4a57be989464f9a1ee1e3481
Immanuel Currah <immanueli.currah@gmail.com>
2022-03-29 22:10:32 -0400

    challenge section done

--- coding-challenges.js
+++ coding-challenges.js
@@ -129,3 +129,24 @@ const minSubarrayLen = (nums, sum) => {
   }
   return minLen === Infinity ? 0 : minLen;
 };
+
+/**
+ * return the longest substring of a string with all unique characters
+ */
+
+const findLongestSubstring = (str) => {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+
+    longest = Math.max(longest, i - start + 1);
+    seen[char] = i + 1;
+  }
+  return longest;
+};
